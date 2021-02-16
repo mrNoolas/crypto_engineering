7efb77aaa323e91cccbcc8d082cded1e9c64c12e
Niek van Sabben <niekvansabben@gmail.com>
2021-02-16 12:29:08 +0100

    added int to string conversion for printing

--- chacha20/chacha20.c
+++ chacha20/chacha20.c
@@ -110,7 +110,9 @@ static const unsigned char sigma[16] = "expand 32-byte k";
 
 int crypto_stream_chacha20(unsigned char *out, unsigned long long outLen, const unsigned char *nonce, const unsigned char *key)
 {
-   send_USART_str(load_test(sigma));
+  char str[25];
+  sprintf(str, "%f", load_test(sigma));
+  send_USART_str(str);
 
 /* testing code
   uint32 p0, p1, p2, p3;
