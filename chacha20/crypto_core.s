cb252ade38001d7a1fc33ae771b026bc5f89b16b
mrNoolas <david@vonk.frl>
2021-02-21 15:35:47 +0100

    Chacha20 moving towards implementing crypto core

---
+++ chacha20/crypto_core.s
@@ -0,0 +1,283 @@
+.syntax unified
+.cpu cortex-m4
+
+.global cryptocore 
+.type  cryptocore, %function
+cryptocore:
+    # =============== crypto core somewhat more efficient ===================
+    # function header in C: uint32 cryptocore(char *out, char *in, char *key, char *c);
+    # r0 is a pointer to the out array: 16 times 32 bits (16 words)
+    # r1 is a pointer to in; 16 chars (16 bytes) (4 words)
+    # r2 is a pointer to the key array (32 chars/bytes) (8 words)
+    # r3 is a pointer to the sigma/c/offset: (16 chars/bytes) (4 words)
+    # The total block length is 64 bytes.
+
+    # Let's first get some room to work in the registers by saving the values in there:
+    push {lr}
+    push {r4-r11}
+
+
+    # We need to allocate memory for x's, and initialise it together with the out. 
+    # allocate 16 uint32's: 16*4 = 64 bytes (block size)
+    # No need to initialise, this is done later together with the j's / out array
+    .data
+    xarray:
+        .skip 64
+    .text
+
+    adr r12, xarray
+
+    # instead of using 16 separate j's as in the example, we write directly to out to save memory operations
+    ldm r3, {r4-r7} 	// load c[0], c[1], c[2], c[3] into r4 to r7
+    ldm r2!, {r8-r11}    // load key[0], ..., key[3] into r8 to r11 
+    
+    # byte reverse the registers we got, (load_littleendian(...))
+    rev r4, r4
+    rev r5, r5
+    rev r6, r6
+    rev r7, r7
+    rev r8, r8
+    rev r9, r9
+    rev r10, r10
+    rev r11, r11
+
+    stm r12!, {r4-r11}	// store in x[0] to x[7]
+    stm r0!, {r4-r11}   // store in out[0] to out[7]
+
+    ldm r2, {r4-r7}	// load key[4], ..., key[7] into r4 to r7
+    ldm r1, {r8-r11}    // load in[0], ..., in[3] into r8 to r11
+    sub r2, #16		// reset pointer to start of key
+    
+    # byte reverse the registers we got, (load_littleendian(...))
+    rev r4, r4
+    rev r5, r5
+    rev r6, r6
+    rev r7, r7
+    rev r8, r8
+    rev r9, r9
+    rev r10, r10
+    rev r11, r11
+
+    stm r12!, {r4-r11}	// store in x[8] to x[15]
+    stm r0!, {r4-r11}    // store in out[8] to out[15]
+
+    
+    
+    
+    
+    # Restore the registers before return:
+    pop {r4-r11}
+    pop {lr}
+    bx lr 
+    
+
+
+
+fullround:
+    # =============== Quarter Round Somewhat more efficient implementation =====================
+    # Our function header in C: void fullround(uint32 *a);
+    # r0 contains &a, pointer to first argument of the list; 15 others directly follow that address.
+
+    mov r12, r0 // keep r0 around for later reference...
+
+    # We need r0 to r7, so push r4-r7 to the stack to preserve them.
+    push {r4-r7}
+
+
+    # Quarter round 1.1 and 1.2:
+    # use pointer to load values from memory:
+    ldr r0, [r12]    	// r0 = x0
+    ldr r1, [r12, #16] 	// r1 = x4
+    ldr r2, [r12, #32] 	// r2 = x8
+    ldr r3, [r12, #48] 	// r3 = x12
+    
+    ldr r4, [r12, #4]  	// r4 = x1
+    ldr r5, [r12, #20] 	// r5 = x5
+    ldr r6, [r12, #36] 	// r6 = x9
+    ldr r7, [r12, #52] 	// r7 = x13
+
+    # two quarter rounds (more info in quarterround/quarterround.s)
+    add r0, r1  	        	// *a = *a + *b
+    add r4, r5  		        // *a = *a + *b
+    eor r3, r0  	        	// *d = *d ^ *a
+    eor r7, r4  		        // *d = *d ^ *a
+    add r2, r2, r3, ror #16 	// *c = *c + *d
+    add r6, r6, r7, ror #16 	// *c = *c + *d
+    eor r1, r2 		        	// *b = *b ^ c
+    eor r5, r6 			        // *b = *b ^ c
+    add r0, r0, r1, ror #20  	// *a = *a + *b
+    add r4, r4, r5, ror #20  	// *a = *a + *b
+    eor r3, r0, r3, ror #16  	// *d = *d ^ a
+    eor r7, r4, r7, ror #16  	// *d = *d ^ a
+    add r2, r2, r3, ror #24	    // *c = *c + *d
+    add r6, r6, r7, ror #24	    // *c = *c + *d
+    eor r1, r2, r1, ror #20 	// *b = *b ^ c
+    eor r5, r6, r5, ror #20 	// *b = *b ^ c
+    
+    ror r3, r3, #24
+    ror r7, r7, #24
+    ror r1, r1, #25
+    ror r5, r5, #25
+
+    # write the results back to memory:
+    str r0, [r12]    	// r0 = x0
+    str r1, [r12, #16] 	// r1 = x4
+    str r2, [r12, #32] 	// r2 = x8
+    str r3, [r12, #48] 	// r3 = x12
+    
+    str r4, [r12, #4]  	// r4 = x1
+    str r5, [r12, #20] 	// r5 = x5
+    str r6, [r12, #36] 	// r6 = x9
+    str r7, [r12, #52] 	// r7 = x13
+
+
+    # Quarter round 1.3 and 1.4:
+    # use pointer to load values from memory:
+    ldr r0, [r12, #8]  	// r0 = x2
+    ldr r1, [r12, #24] 	// r1 = x6
+    ldr r2, [r12, #40] 	// r2 = x10
+    ldr r3, [r12, #56] 	// r3 = x14
+    
+    ldr r4, [r12, #12] 	// r4 = x3
+    ldr r5, [r12, #28] 	// r5 = x7
+    ldr r6, [r12, #44] 	// r6 = x11
+    ldr r7, [r12, #60] 	// r7 = x15
+
+    # two quarter rounds (more info in quarterround/quarterround.s)
+    add r0, r1  	        	// *a = *a + *b
+    add r4, r5  		        // *a = *a + *b
+    eor r3, r0  	        	// *d = *d ^ *a
+    eor r7, r4  		        // *d = *d ^ *a
+    add r2, r2, r3, ror #16 	// *c = *c + *d
+    add r6, r6, r7, ror #16 	// *c = *c + *d
+    eor r1, r2 		        	// *b = *b ^ c
+    eor r5, r6 			        // *b = *b ^ c
+    add r0, r0, r1, ror #20  	// *a = *a + *b
+    add r4, r4, r5, ror #20  	// *a = *a + *b
+    eor r3, r0, r3, ror #16  	// *d = *d ^ a
+    eor r7, r4, r7, ror #16  	// *d = *d ^ a
+    add r2, r2, r3, ror #24	    // *c = *c + *d
+    add r6, r6, r7, ror #24	    // *c = *c + *d
+    eor r1, r2, r1, ror #20 	// *b = *b ^ c
+    eor r5, r6, r5, ror #20 	// *b = *b ^ c
+    
+    ror r7, r7, #24
+    ror r3, r3, #24
+    ror r5, r5, #25
+    ror r1, r1, #25
+
+    # write the results back to memory:
+    str r0, [r12, #8]  	// r0 = x2
+    str r3, [r12, #56] 	// r3 = x14
+    
+    str r4, [r12, #12] 	// r4 = x3
+    str r5, [r12, #28] 	// r5 = x7
+
+
+    # r2 and r6 should stay where they are for round 2  (r2 = x10; r6 = x11)
+    mov r5, r1 	// r5 = x6
+    mov r3, r7  // r3 = x15
+    
+    
+    # =======================================================
+    # Quarter round 2.1 and 2.2:
+    # use pointer to load values from memory:
+    ldr r0, [r12]    	// r0 = x0
+    ldr r1, [r12, #20] 	// r1 = x5
+    
+    ldr r4, [r12, #4]  	// r4 = x1
+    ldr r7, [r12, #48] 	// r7 = x12
+
+    # two quarter rounds (more info in quarterround/quarterround.s)
+    add r0, r1  	        	// *a = *a + *b
+    add r4, r5  		        // *a = *a + *b
+    eor r3, r0  	        	// *d = *d ^ *a
+    eor r7, r4  		        // *d = *d ^ *a
+    add r2, r2, r3, ror #16 	// *c = *c + *d
+    add r6, r6, r7, ror #16 	// *c = *c + *d
+    eor r1, r2 		        	// *b = *b ^ c
+    eor r5, r6 			        // *b = *b ^ c
+    add r0, r0, r1, ror #20  	// *a = *a + *b
+    add r4, r4, r5, ror #20  	// *a = *a + *b
+    eor r3, r0, r3, ror #16  	// *d = *d ^ a
+    eor r7, r4, r7, ror #16  	// *d = *d ^ a
+    add r2, r2, r3, ror #24	    // *c = *c + *d
+    add r6, r6, r7, ror #24	    // *c = *c + *d
+    eor r1, r2, r1, ror #20 	// *b = *b ^ c
+    eor r5, r6, r5, ror #20 	// *b = *b ^ c
+    
+    ror r7, r7, #24
+    ror r3, r3, #24
+    ror r5, r5, #25
+    ror r1, r1, #25
+
+    # write the results back to memory:
+    str r0, [r12]    	// r0 = x0
+    str r1, [r12, #20] 	// r1 = x5
+    str r2, [r12, #40] 	// r2 = x10
+    str r3, [r12, #60] 	// r3 = x15
+    
+    str r4, [r12, #4]  	// r4 = x1
+    str r5, [r12, #24] 	// r5 = x6
+    str r6, [r12, #44] 	// r6 = x11
+    str r7, [r12, #48] 	// r7 = x12   
+
+
+
+    # Quarter round 2.3 and 2.4:
+    # use pointer to load values from memory:
+    ldr r0, [r12, #8]  	// r0 = x2
+    ldr r1, [r12, #28] 	// r1 = x7
+    ldr r2, [r12, #32] 	// r2 = x8
+    ldr r3, [r12, #52] 	// r3 = x13
+    
+    ldr r4, [r12, #12] 	// r4 = x3
+    ldr r5, [r12, #16] 	// r5 = x4
+    ldr r6, [r12, #36] 	// r6 = x9
+    ldr r7, [r12, #56] 	// r7 = x14
+    
+    # retrieve the four we stored earlier:
+    //mov r0, r8  // r0 = x2
+    //mov r7, r9  // r7 = x14
+    //mov r4, r10 // r4 = x3
+    //mov r1, r11 // r1 = x7
+
+    # two quarter rounds (more info in quarterround/quarterround.s)
+    add r0, r1  	        	// *a = *a + *b
+    add r4, r5  		        // *a = *a + *b
+    eor r3, r0  	        	// *d = *d ^ *a
+    eor r7, r4  		        // *d = *d ^ *a
+    add r2, r2, r3, ror #16 	// *c = *c + *d
+    add r6, r6, r7, ror #16 	// *c = *c + *d
+    eor r1, r2 		        	// *b = *b ^ c
+    eor r5, r6 			        // *b = *b ^ c
+    add r0, r0, r1, ror #20  	// *a = *a + *b
+    add r4, r4, r5, ror #20  	// *a = *a + *b
+    eor r3, r0, r3, ror #16  	// *d = *d ^ a
+    eor r7, r4, r7, ror #16  	// *d = *d ^ a
+    add r2, r2, r3, ror #24	    // *c = *c + *d
+    add r6, r6, r7, ror #24	    // *c = *c + *d
+    eor r1, r2, r1, ror #20 	// *b = *b ^ c
+    eor r5, r6, r5, ror #20 	// *b = *b ^ c
+    
+    ror r7, r7, #24
+    ror r3, r3, #24
+    ror r5, r5, #25
+    ror r1, r1, #25
+    
+    # write the results back to memory:
+    str r0, [r12, #8]  	// r0 = x2
+    str r1, [r12, #28] 	// r1 = x7
+    str r2, [r12, #32] 	// r2 = x8
+    str r3, [r12, #52] 	// r3 = x13
+    
+    str r4, [r12, #12] 	// r4 = x3
+    str r5, [r12, #16] 	// r5 = x4
+    str r6, [r12, #36] 	// r6 = x9
+    str r7, [r12, #56] 	// r7 = x14
+
+
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r7}
+    bx lr
+
---
+++ chacha20/doubleround/chacha20.c
@@ -0,0 +1,153 @@
+/* Based on the public domain implemntation in
+ * crypto_stream/chacha20/e/ref from http://bench.cr.yp.to/supercop.html
+ * by Daniel J. Bernstein */
+
+#include <stdint.h>
+#include "chacha20.h"
+
+#define ROUNDS 20
+
+typedef uint32_t uint32;
+
+extern uint32 fullround(uint32 *a);
+
+//extern uint32 quarterround(uint32 *a);//, uint32 *b, uint32 *c, uint32 *d);
+
+static uint32 load_littleendian(const unsigned char *x)
+{
+  return
+      (uint32) (x[0]) \
+  | (((uint32) (x[1])) << 8) \
+  | (((uint32) (x[2])) << 16) \
+  | (((uint32) (x[3])) << 24);
+}
+
+static void store_littleendian(unsigned char *x,uint32 u)
+{
+  x[0] = u; u >>= 8;
+  x[1] = u; u >>= 8;
+  x[2] = u; u >>= 8;
+  x[3] = u;
+}
+
+static int crypto_core_chacha20(
+        unsigned char *out,
+  const unsigned char *in,
+  const unsigned char *key,
+  const unsigned char *c
+)
+{
+  uint32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
+  uint32 j0, j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12, j13, j14, j15;
+  int i;
+
+  j0  = x0  = load_littleendian(c +  0);
+  j1  = x1  = load_littleendian(c +  4);
+  j2  = x2  = load_littleendian(c +  8);
+  j3  = x3  = load_littleendian(c + 12);
+  j4  = x4  = load_littleendian(key +  0);
+  j5  = x5  = load_littleendian(key +  4);
+  j6  = x6  = load_littleendian(key +  8);
+  j7  = x7  = load_littleendian(key + 12);
+  j8  = x8  = load_littleendian(key + 16);
+  j9  = x9  = load_littleendian(key + 20);
+  j10 = x10 = load_littleendian(key + 24);
+  j11 = x11 = load_littleendian(key + 28);
+  j12 = x12 = load_littleendian(in +  8);
+  j13 = x13 = load_littleendian(in + 12);
+  j14 = x14 = load_littleendian(in +  0);
+  j15 = x15 = load_littleendian(in +  4);
+
+  uint32 x[16] = {x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15};
+  for (i = ROUNDS; i > 0; i -= 2) {
+    fullround(x);
+  }
+
+  x[0] += j0;
+  x[1] += j1;
+  x[2] += j2;
+  x[3] += j3;
+  x[4] += j4;
+  x[5] += j5;
+  x[6] += j6;
+  x[7] += j7;
+  x[8] += j8;
+  x[9] += j9;
+  x[10] += j10;
+  x[11] += j11;
+  x[12] += j12;
+  x[13] += j13;
+  x[14] += j14;
+  x[15] += j15;
+
+  store_littleendian(out + 0,x[0]);
+  store_littleendian(out + 4,x[1]);
+  store_littleendian(out + 8,x[2]);
+  store_littleendian(out + 12,x[3]);
+  store_littleendian(out + 16,x[4]);
+  store_littleendian(out + 20,x[5]);
+  store_littleendian(out + 24,x[6]);
+  store_littleendian(out + 28,x[7]);
+  store_littleendian(out + 32,x[8]);
+  store_littleendian(out + 36,x[9]);
+  store_littleendian(out + 40,x[10]);
+  store_littleendian(out + 44,x[11]);
+  store_littleendian(out + 48,x[12]);
+  store_littleendian(out + 52,x[13]);
+  store_littleendian(out + 56,x[14]);
+  store_littleendian(out + 60,x[15]);
+
+  return 0;
+}
+
+static const unsigned char sigma[16] = "expand 32-byte k";
+
+int crypto_stream_chacha20(unsigned char *out, unsigned long long outLen, const unsigned char *nonce, const unsigned char *key)
+{
+/* testing code
+  uint32 p[4];
+  p[0] = 1;
+  p[1] = 2;
+  p[2] = 3;
+  p[3] = 4;
+  unsigned char blob[5] = {p[0] + '0', p[1] + '0',  p[2] + '0', p[3] + '0', '\0'};
+  send_USART_str(blob);
+
+  quarterround(&p);
+
+  unsigned char blob1[5] = {p[0] + '0', p[1] + '0',  p[2] + '0', p[3] + '0', '\0'};
+  send_USART_str(blob1);
+ //*/ 
+
+  unsigned char in[16];
+  unsigned char block[64];
+  unsigned char keyCopy[32];
+  unsigned long long i;
+  unsigned int u;
+
+  if (!outLen) return 0;
+
+  for (i = 0; i < 32; ++i) keyCopy[i] = key[i];
+  for (i = 0; i < 8; ++i) in[i] = nonce[i];
+  for (i = 8; i < 16; ++i) in[i] = 0;
+
+  while (outLen >= 64) {
+    crypto_core_chacha20(out, in, keyCopy, sigma);
+
+    u = 1;
+    for (i = 8; i < 16; ++i) {
+      u += (unsigned int) in[i];
+      in[i] = u;
+      u >>= 8;
+    }
+
+    outLen -= 64;
+    out += 64;
+  }
+
+  if (outLen) {
+    crypto_core_chacha20(block, in, keyCopy, sigma);
+    for (i = 0; i < outLen; ++i) out[i] = block[i];
+  }
+  return 0;
+}
