9ea95a878871d407d0e77fe3ff2d0cd14c932436
mrNoolas <david@vonk.frl>
2021-03-26 14:08:29 +0100

    working version 16322 cycles

--- poly1305/poly1305.c
+++ poly1305/poly1305.c
@@ -122,7 +122,6 @@ static const unsigned int minusp[6] = {5, 0, 0, 0, 0, 0xffffffff} ;
 
 int crypto_onetimeauth_poly1305(unsigned char *out_mac,const unsigned char *in_msg,unsigned long long inlen,const unsigned char *key)
 {
-  unsigned int j;
   unsigned int c[6], h[6], r[6];
   
   r[0] = ((key[0]  & 0b11111111) >> 0) | (key[1] << 8)         | (key[2] << 16)         | ((key[3]  & 0b00000011) << 24);
@@ -132,68 +131,76 @@ int crypto_onetimeauth_poly1305(unsigned char *out_mac,const unsigned char *in_m
   r[4] = ((key[13] & 0b11111111) >> 0) | (key[14] << 8)        | ((key[15] & 15) << 16);
   r[5] = 0;
 
-  for (j = 0;j < 6;++j) h[j] = 0;
+  h[0] = 0; h[1] = 0; h[2] = 0; h[3] = 0; h[4] = 0; h[5] = 0;
 
   while (inlen > 0) {
-    for (j = 0;j < 6;++j) c[j] = 0;
-
     // copy input block
-    if (inlen >= 16) c[4] |= (1 << 24);
-    if (inlen == 15) c[4] |= 1 << 16; else if (inlen > 15) c[4] |= in_msg[15] << 16;
-    if (inlen == 14) c[4] |= 1 << 8;  else if (inlen > 14) c[4] |= in_msg[14] << 8;
-    if (inlen == 13) c[4] |= 1;       else if (inlen > 13) c[4] |= in_msg[13];
-    if (inlen == 12) c[3] |= 1 << 18; else if (inlen > 12) c[3] |= in_msg[12] << 18;
-    if (inlen == 11) c[3] |= 1 << 10; else if (inlen > 11) c[3] |= in_msg[11] << 10;
-    if (inlen == 10) c[3] |= 1 << 2;  else if (inlen > 10) c[3] |= in_msg[10] << 2;
-    if (inlen == 9) {
-        c[2] |= 1 << 20;
-    } else if (inlen > 9) {
-        c[3] |= (in_msg[9] & 0b11000000) >> 6;
-        c[2] |= (in_msg[9] & 0b00111111) << 20;
-    }
-    if (inlen == 8) c[2] |= 1 << 12; else if (inlen > 8) c[2] |= in_msg[8] << 12;
-    if (inlen == 7) c[2] |= 1 << 4;  else if (inlen > 7) c[2] |= in_msg[7] << 4;
-    if (inlen == 6) {
-        c[1] |= 1 << 22;
-    } else if (inlen > 6) {
-        c[2] |= (in_msg[6] & 0b11110000) >> 4;
-        c[1] |= (in_msg[6] & 0b00001111) << 22;
-    }
-    if (inlen == 5) c[1] |= 1 << 14; else if (inlen > 5) c[1] |= in_msg[5] << 14;
-    if (inlen == 4) c[1] |= 1 << 6;  else if (inlen > 4) c[1] |= in_msg[4] << 6;
-    if (inlen == 3) {
-        c[0] |= 1 << 24;
-    } else if (inlen > 3) {
-        c[1] |= (in_msg[3] & 0b11111100) >> 2;
-        c[0] |= (in_msg[3] & 0b00000011) << 24;
-    }
-    if (inlen == 2) c[0] |= 1 << 16; else if (inlen > 2) c[0] |= in_msg[2] << 16;
-    if (inlen == 1) c[0] |= 1 << 8; else if (inlen > 1) c[0] |= in_msg[1] << 8;
-    c[0] |= in_msg[0];
-     
-    if (inlen < 16) {
-        in_msg += inlen;
-        inlen -= inlen;
-    } else {
+    c[0] = in_msg[0]; c[1] = 0; c[2] = 0; c[3] = 0; c[4] = 0; c[5] = 0; // reset c
+    if (inlen >= 16) { // most common case
+        c[0] |= in_msg[1] << 8 | in_msg[2] << 16 | (in_msg[3] & 0b00000011) << 24;
+        c[1] = (in_msg[3] & 0b11111100) >> 2 | in_msg[4] << 6 | in_msg[5] << 14 | (in_msg[6] & 0b00001111) << 22;
+        c[2] = (in_msg[6] & 0b11110000) >> 4 | in_msg[7] << 4 | in_msg[8] << 12 | (in_msg[9] & 0b00111111) << 20;
+        c[3] = (in_msg[9] & 0b11000000) >> 6 | in_msg[10] << 2 | in_msg[11] << 10 | in_msg[12] << 18;
+        c[4] = in_msg[13] | in_msg[14] << 8 | in_msg[15] << 16 | (1 << 24);
+        
         in_msg += 16;
         inlen -= 16;
+        add(h, c);    
+        mulmod(h,r);
+    } else { 
+        if (inlen == 1) c[0] |= 1 << 8; else {
+            c[0] |= in_msg[1] << 8;
+            if (inlen == 2) c[0] |= 1 << 16; else {
+                c[0] |= in_msg[2] << 16;
+                if (inlen == 3) c[0] |= 1 << 24; else {
+                    c[1] |= (in_msg[3] & 0b11111100) >> 2;
+                    c[0] |= (in_msg[3] & 0b00000011) << 24;
+                    if (inlen == 4) c[1] |= 1 << 6;  else {
+                        c[1] |= in_msg[4] << 6;
+                        if (inlen == 5) c[1] |= 1 << 14; else {
+                            c[1] |= in_msg[5] << 14;
+                            if (inlen == 6) c[1] |= 1 << 22; else  {
+                                c[2] |= (in_msg[6] & 0b11110000) >> 4;
+                                c[1] |= (in_msg[6] & 0b00001111) << 22;
+                                if (inlen == 7) c[2] |= 1 << 4;  else {
+                                    c[2] |= in_msg[7] << 4;
+                                    if (inlen == 8) c[2] |= 1 << 12; else {
+                                        c[2] |= in_msg[8] << 12;
+                                        if (inlen == 9) c[2] |= 1 << 20; else {
+                                            c[3] |= (in_msg[9] & 0b11000000) >> 6;
+                                            c[2] |= (in_msg[9] & 0b00111111) << 20;
+                                            if (inlen == 10) c[3] |= 1 << 2;  else {
+                                                c[3] |= in_msg[10] << 2;
+                                                if (inlen == 11) c[3] |= 1 << 10; else {
+                                                    c[3] |= in_msg[11] << 10;
+                                                    if (inlen == 12) c[3] |= 1 << 18; else {
+                                                        c[3] |= in_msg[12] << 18;
+                                                        if (inlen == 13) c[4] |= 1; else {
+                                                            c[4] |= in_msg[13];
+                                                            if (inlen == 14) c[4] |= 1 << 8; else { // inlen == 15
+                                                                c[4] |= in_msg[14] << 8;
+                                                                c[4] |= 1 << 16;
+                                                                    
+        }}}}}}}}}}}}}}
+        in_msg += inlen;
+        inlen -= inlen;
+        add(h, c);    
+        mulmod(h,r);
     }
-
-    add(h, c);    
-    mulmod(h,r);
   }
   
   // freeze 
   // unsigned int horig[6]; use r, since its no longer needed now
   unsigned int negative;
-  for (j = 0;j < 6;++j) {
-    r[j] = h[j];
-  }
+  r[0] = h[0]; r[1] = h[1]; r[2] = h[2]; r[3] = h[3]; r[4] = h[4]; r[5] = h[5];
   add(h,minusp);
   negative = -(h[5] >> 25);
-  for (j = 0;j < 6;++j) {
-    h[j] ^= negative & (r[j] ^ h[j]);
-  }
+  h[0] ^= negative & (r[0] ^ h[0]);
+  h[1] ^= negative & (r[1] ^ h[1]);
+  h[2] ^= negative & (r[2] ^ h[2]);
+  h[3] ^= negative & (r[3] ^ h[3]);
+  h[4] ^= negative & (r[4] ^ h[4]);
+  h[5] ^= negative & (r[5] ^ h[5]);
   // end of freeze
   
   c[0] = ((key[16] & 0b11111111) >> 0) | (key[17] << 8) | (key[18] << 16) | ((key[19]  & 0b00000011) << 24);
